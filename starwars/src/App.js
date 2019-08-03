297c04940232fdbf5ee9efe19c8472968b48db5e
LittleBoGeek <NadiaChilds@outlook.com>
2019-08-03 00:18:05 -0400

    wip

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+
+  axios.get("'http https://swapi.co/api/people/1/'").then( response => {
+    console.log("success!", response)
+  })
+  .catch( err =>{
+    console.log("error:", err)
+  
+  })
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +18,15 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
+  return ( 
+ <div className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+     
+    </div> 
   );
 }
 
+
+
 export default App;
+/*https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js */
